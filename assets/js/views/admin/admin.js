fa66d24865285dffb8ed32f91d86aef7d7ebcebb
kazeidesign <contact@kazeidesign.fr>
2016-05-25 10:09:30 +0000

    Display Post in Admin Space
    Display Post in Admin Space

--- assets/js/views/admin/admin.js
+++ assets/js/views/admin/admin.js
@@ -9,9 +9,33 @@ angular.module('myApp.admin', ['ngRoute'])
     templateUrl: 'admin/admin.html',
     controller: 'AdminCtrl'
   })
+  
+  .when('/admin/blog', {
+    templateUrl: 'admin/admin_blog.html',
+    controller: 'BlogCtrl as post'
+  })
+  
+  .when('/admin/blog/:title', {
+    templateUrl: 'admin/admin_blogDetail.html',
+    controller: 'AdminBlogDetailCtrl as post'
+  })
   ;
 }])
 
 .controller('AdminCtrl', [function() {
 
-}]);
+}])
+
+.controller('AdminBlogDetailCtrl', function ($rootScope, sailsResource, $location) {
+  
+  var self = this;
+  
+  // Acces to a post
+  var postUrlToTitle = $location.path().split("/");
+  var postTitle = postUrlToTitle[3].split('_').join(' ');
+  
+  var blogPostTitle = sailsResource('Blog').get({ title: postTitle });
+
+  this.blogPost = blogPostTitle;
+  
+})
